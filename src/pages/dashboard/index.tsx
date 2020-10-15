74b97083f314d3c6c26d422c49cd7e2f75d84aa4
root <root@mr-notebook.home>
2020-10-15 10:37:47 -0300

    exit

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
 import Logo from '../../img/gitYuri.png';
+
+import Carousel3 from '../../img/boa3.jpeg';
+
 import { Container, Header, Content} from './styles';
 
 const dashboard = () => {
@@ -7,7 +11,9 @@ const dashboard = () => {
         <>
             <Container>
                 <Header>
-                    <h1>logo</h1>
+                    <div className="logo">
+                        <img src={Logo} alt="Logo" height="100px"/>
+                    </div>
                     <ul>
                         <a href="">Home</a>
                         <a href="">Sobre</a>
@@ -15,7 +21,17 @@ const dashboard = () => {
                     </ul>
                 </Header>
                 <Content>
-                    
+                    <div className="press">
+                        <input type="text" placeholder="Digite Aqui"/>
+                        <button>Press</button>
+                    </div>
+
+                    <div className="boa3">
+                            <img src={Carousel3} alt="boa3" width="80%"/>
+                    </div>     
+                    <small>Ignora a aleatoriedade e péssima imagem </small> 
+
+                   
                 </Content>
             </Container>
         </>
--- src/pages/dashboard/styles.tsx
+++ src/pages/dashboard/styles.tsx
@@ -8,25 +8,78 @@ export const  Container = styled.div`
 
 export const Header = styled.div`
     background: #000;
-    h1{
-        color: #fff;
-        padding: 1.5rem;
+    .logo{
+        background: #fff;
         display: flex;
         justify-content: center;
     }
 
     ul{
-        border-top: 2px solid red;
+        border-top: 2px solid #fff;
         display: flex; 
         justify-content: center;
         a{
-            color: red;
+            color: #fff;
             text-decoration: none;
             padding: 0.8rem 1.5rem;
             font-size: 1.2rem;
+            font-weight: bold;
+        }
+        
+        a:hover{
+            color: red;
         }
     }
 `;
 
 export const Content = styled.div`
+    .press{
+        display: flex;
+        justify-content: center;
+            input{
+                border-top: solid;
+                border-bottom: none;
+                border-right: none;
+                border-left: none;
+                border-radius: 5px;
+                height: 2.5rem;
+                width: 18rem;
+                color: #000;
+                text-align: center;
+                font-size: 1.2rem;
+                font-weight: bold;
+                margin-right: 2.0rem;
+            }
+
+            input::placeholder{ 
+                color: #000;
+                font-size: 1.2rem;
+                font-weight: bold;
+            }
+
+            button{
+                height: 2.2rem;
+                width: 5.0rem;
+                color: #fff;
+                background: #000;
+                border-radius: 15px;
+                outline: 0;
+            }
+
+            button:hover{
+                color: red;
+            }
+    }
+
+    .boa3{
+        display: flex;
+        justify-content: center;
+        margin-top: 2.0rem;
+        
+    }   
+
+    small{
+        display: flex;
+        justify-content: center;
+    }
 `;
